432ada5a1cb43ac8f1df11947ea6de78221cc6d6
kryptocodes <srivatsantb@gmail.com>
2021-05-31 16:49:13 +0530

    updated resume
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/pages/snippets.js
+++ src/pages/snippets.js
@@ -6,7 +6,6 @@ const Snippets = () => {
       <Wrapper title="Snippets">
         <div className="container text-white pt-4">
         <h1>Snippets</h1>
-        <p>Snippets are used to share things when I am lazy to take the effort to make it a project or a blog post.</p>
         </div>
     </Wrapper>
   )
