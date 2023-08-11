c50f540f6a1527859404ad2b79326cf2f6cbf61d
kryptocodes <srivatsantb@gmail.com>
2023-08-12 00:40:26 +0530

    Added new files
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- components/ENV.ts
+++
@@ -1,2 +0,0 @@
-export const NOTION_BLOG_ID = 'e92fcd11a75f4e9da334e22e291cbc02'
-export const NOTION_PROJECT_ID = '96b02c4f04d84879b149a9f9ab3ed6e6'
--- next.config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-
-module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  }
-}
